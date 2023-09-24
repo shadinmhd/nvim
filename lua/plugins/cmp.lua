5518428db46aa3c7355a40394fe6d90d8071e5b6
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-09-24 10:32:07 +0530

    i dont remember what i did

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,8 @@
 return {
 	'VonHeikemen/lsp-zero.nvim',
 	dependencies = {
-		'neovim/nvim-lspconfig','hrsh7th/cmp-nvim-lsp','hrsh7th/nvim-cmp','L3MON4D3/LuaSnip','williamboman/mason.nvim','williamboman/mason-lspconfig.nvim'
+		'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/nvim-cmp', 'L3MON4D3/LuaSnip', 'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim'
 	},
 	config = function()
 		local lsp_zero = require('lsp-zero')
@@ -9,7 +10,7 @@ return {
 		lsp_zero.on_attach(function(client, bufnr)
 			-- see :help lsp-zero-keybindings
 			-- to learn the available actions
-			lsp_zero.default_keymaps({buffer = bufnr})
+			lsp_zero.default_keymaps({ buffer = bufnr })
 		end)
 
 		local cmp = require("cmp")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 	tag = '0.1.3',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		require('telescope').setup { defaults = { file_ignore_patterns = { "node_modules", "git" } } }
+		require('telescope').setup { defaults = { file_ignore_patterns = { "node_modules", "git" , "debug", "Debug"} } }
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 	end
