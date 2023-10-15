482bf936d0b4ee4c028c4017411cc466389ae466
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-10-15 12:07:43 +0530

    added comments toggleterm whichkey

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,3 +1,15 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>q", ":qa<CR>")
+vim.keymap.set("n", "<leader>w", ":wa<CR>")
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
+
+vim.keymap.set('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
+
+vim.keymap.set('t', '<A-j>', '<C-\\><C-N><C-w>j', { noremap = true, silent = true })
+vim.keymap.set('t', '<A-k>', '<C-\\><C-N><C-w>k', { noremap = true, silent = true })
+vim.keymap.set('t', '<A-l>', '<C-\\><C-N><C-w>l', { noremap = true, silent = true })
+vim.keymap.set('t', '<A-h>', '<C-\\><C-N><C-w>h', { noremap = true, silent = true })
