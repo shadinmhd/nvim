9d796e9e8a7b8ff8be09a6820dfaf46651c434c0
shadinmhd <shadinmhd97@gmail.com>
2024-02-25 13:37:43 +0530

    update: added vim-tmux-navigator

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,18 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<A-h>",  "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<A-j>",  "<cmd>TmuxNavigateDown<cr>" },
+		{ "<A-k>",  "<cmd>TmuxNavigateUp<cr>" },
+		{ "<A-l>",  "<cmd>TmuxNavigateRight<cr>" },
+		{ "<A-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+	},
+}
