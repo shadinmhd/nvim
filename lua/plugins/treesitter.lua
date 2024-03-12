e90c62d80dab8c800cb3fe8157c8a6c0976fff7a
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-03-12 15:50:14 +0530

    update: added flutter tools to neovim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = { "windwp/nvim-ts-autotag" },
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		require("nvim-ts-autotag").setup({})
