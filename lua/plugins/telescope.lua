22a10a5695f5708bbd6007fd70d865311356416d
shadinmhd <shadinmhd97@gmail.com>
2024-04-03 19:14:09 +0530

    update: added keybinding for live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,5 +24,6 @@ return {
 		}
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
 	end
 }
