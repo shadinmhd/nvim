ba51b21aaa4d6dcda649e7a6c61887121a4ba004
shadinmhd <shadinmhd97@gmail.com>
2024-04-12 07:58:52 +0530

    update: added couple of file formats to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 		require("nvim-ts-autotag").setup({})
 
 		configs.setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "html" },
+			ensure_installed = { "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "html", "markdown", "yaml", "json" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
