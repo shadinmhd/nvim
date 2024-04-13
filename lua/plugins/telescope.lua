41b0fd31d40f0677a8b51d53b82943c32cdd9a03
shadinmhd <shadinmhd97@gmail.com>
2024-04-13 20:25:31 +0530

    update: added keybinding for buffers and help_tags in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,5 +25,7 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+		vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
 	end
 }
